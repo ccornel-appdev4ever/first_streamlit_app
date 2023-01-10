5a7e3759e59ade9796f930be8ad734c54a2e600f
Christian Cornel <122361812+ccornel-appdev4ever@users.noreply.github.com>
2023-01-10 22:34:49 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.title('Breakfast Menu')
+streamlit.title('Omega 3 & Blueberry Smoothie')
+streamlit.title('Hard-Boiled Free-Range Egg')
