3b1969e0039287dc9027b7a2285a92fe82dd18b6
Christian Cornel <122361812+ccornel-appdev4ever@users.noreply.github.com>
2023-01-10 22:36:11 +0800

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
-streamlit.title('Breakfast Menu')
-streamlit.title('Omega 3 & Blueberry Smoothie')
-streamlit.title('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Smoothie')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
